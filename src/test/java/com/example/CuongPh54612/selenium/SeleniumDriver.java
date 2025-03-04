cbb8aaf3216fd14ce9875e30c912d76fc64b7670
Cuong <duccuongkun@gmail.com>
2025-03-04 15:03:13 +0700

    updateDone

--- src/test/java/com/example/CuongPh54612/selenium/SeleniumDriver.java
+++ src/test/java/com/example/CuongPh54612/selenium/SeleniumDriver.java
@@ -1,2 +1,35 @@
-package com.example.CuongPh54612.selenium;public class SeleniumDriver {
+package com.example.CuongPh54612.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumDriver {
+    private WebDriver webDriver;
+
+    @BeforeEach
+    void setUp() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+    }
+
+
+    @Test
+    void stepUp() throws InterruptedException {
+        webDriver.get("https://github.com/");
+
+        Thread.sleep(2000);
+        webDriver.findElement(By.xpath("/html/body/div[1]/div[3]/header/div/div[2]/div/div/div/a")).click();
+        Thread.sleep(2000);
+        webDriver.findElement(By.xpath("//*[@id=\"login_field\"]")).sendKeys("duccuongkun@gmail.com");
+        Thread.sleep(2000);
+        webDriver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("029927182cuong");
+        Thread.sleep(2000);
+        webDriver.findElement(By.xpath("//*[@id=\"login\"]/div[4]/form/div/input[13]")).click();
+
+
+    }
 }
